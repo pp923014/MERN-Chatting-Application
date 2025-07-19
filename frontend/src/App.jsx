b359c615026476e806c8c708e3dc92c20559f001
priyanshu Kumar Pandey <06priyanshupandey@gmail.com>
2025-07-19 21:06:47 +0530

    first commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="h-screen bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 flex items-center justify-center">
+      <h1 className="text-4xl font-bold text-white bg-red-500">Tailwind is working!</h1>
+    </div>
+  );
+}
+
+export default App;
